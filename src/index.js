8b5c6bf6f99958dca146f28b5a68ab7150753274
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-20 14:26:07 -0300

    feat: global styles with styled-components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import Login from './containers/Login'
+import GlobalStyles from './styles/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Login />, document.getElementById('root'))
+root.render(
+  <>
+    <Login />
+    <GlobalStyles />
+  </>,
+  document.getElementById('root')
+)
