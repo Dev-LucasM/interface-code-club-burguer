7122b238eca2c45b8da97421771b441c78308d48
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-20 16:34:12 -0300

    feat: login page created

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
+import LoginImg from '../../assets/login-image.svg'
+import Logo from '../../assets/logo.svg'
+import {
+  Container,
+  LoginImage,
+  ContainerItens,
+  Label,
+  Input,
+  Button,
+  SignInLink
+} from './styles'
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LoginImage src={LoginImg} alt="login-image" />
+      <ContainerItens>
+        <img src={Logo} alt="logo-code-burguer" />
+        <h1>Login</h1>
+
+        <Label>Email</Label>
+        <Input />
+
+        <Label>Senha</Label>
+        <Input />
+
+        <Button>Entrar</Button>
+        <SignInLink>
+          Não possui conta ? <a>Cadastre-se</a>
+        </SignInLink>
+      </ContainerItens>
+    </Container>
   )
 }
 
