f91aabef480056d970375b8ff152c6956ba43093
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-20 17:53:26 -0300

    feat: validating login form

--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -60,7 +60,7 @@ export const Input = styled.input`
   background: #ffffff;
   box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
   border-radius: 5px;
-  border: none;
+  border: ${props => (props.error ? '2px solid #CC1717' : 'none')};
   padding-left: 10px;
 `
 
@@ -108,4 +108,13 @@ export const SignInLink = styled.p`
       opacity: 0.6;
     }
   }
+`
+
+export const ErrorMessage = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  color: #cc1717;
+  margin-top: 2px;
 `
