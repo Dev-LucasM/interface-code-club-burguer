b0d500fb08da58ad736741c81595caa2f787d7b9
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-28 18:28:43 -0300

    feat: styling the cart

--- src/components/CartItems/styles.js
+++ src/components/CartItems/styles.js
@@ -1,7 +1,46 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  background-color: #ffffff;
+  border-radius: 20px;
+  box-shadow: 0px 10px 40px 0px rgba(0, 0, 0, 0.03);
+  padding: 10px;
+  width: max-content;
+`
 
-export const Header = styled.div``
+export const Header = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  padding: 10px;
+  border-bottom: 1px solid #b5b5b5;
 
-export const Body = styled.div``
+  p {
+    font-size: 16px;
+    color: #b5b5b5;
+  }
+`
+
+export const Body = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  width: max-content;
+  padding: 10px;
+  grid-gap: 10px 15px;
+
+  img {
+    border-radius: 10px;
+    width: 120px;
+  }
+
+  p {
+    font-size: 16px;
+    color: #000;
+    font-weight: 400;
+  }
+`
+
+export const EmptyCart = styled.div`
+  padding: 20px;
+  text-align: center;
+  font-weight: bold;
+`
