6225d2ff4b7e5d095a6afabf3594ac923a9487ab
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-28 16:54:52 -0300

    feat: creating cart logic

--- src/components/CardProduct/index.js
+++ src/components/CardProduct/index.js
@@ -2,17 +2,20 @@ import React from 'react'
 
 import PropTypes from 'prop-types'
 
+import { useCart } from '../../hooks/CartContex'
 import { Button } from '../Button'
 import { Container, Image, ProductName, ProductPrice } from './styles'
 
 export function CardProduct({ product }) {
+  const { putProductInCart } = useCart()
+
   return (
     <Container>
       <Image src={product.url} alt="imagem do produto" />
       <div>
         <ProductName>{product.name}</ProductName>
         <ProductPrice>{product.formatedPrice}</ProductPrice>
-        <Button>Adicionar</Button>
+        <Button onClick={() => putProductInCart(product)}>Adicionar</Button>
       </div>
     </Container>
   )
