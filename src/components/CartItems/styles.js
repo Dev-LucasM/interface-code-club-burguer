119bd9fe39907c759b34004d9044e8d1ca7aca5d
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-28 18:53:44 -0300

    feat: styling quantity buttons

--- src/components/CartItems/styles.js
+++ src/components/CartItems/styles.js
@@ -37,6 +37,23 @@ export const Body = styled.div`
     color: #000;
     font-weight: 400;
   }
+
+  .quantity-container {
+    display: flex;
+    gap: 20px;
+
+    button {
+      height: 30px;
+      background: transparent;
+      border: none;
+      font-size: 24px;
+      cursor: pointer;
+    }
+
+    p {
+      margin-top: 5px;
+    }
+  }
 `
 
 export const EmptyCart = styled.div`
