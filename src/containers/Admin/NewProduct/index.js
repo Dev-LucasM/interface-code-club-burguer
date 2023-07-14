0ae90430eafe19677082dbc7b33d95b23ea93960
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-07-14 20:09:06 -0300

    feat: validating product creation form

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -2,15 +2,43 @@ import React, { useEffect, useState } from 'react'
 import { useForm, Controller } from 'react-hook-form'
 import ReactSelect from 'react-select'
 
+import { yupResolver } from '@hookform/resolvers/yup'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
+import * as Yup from 'yup'
 
+import { ErrorMessage } from '../../../components'
 import api from '../../../services/api'
 import { Container, Label, LabelUpload, Input, ButtonStyles } from './styles'
 
 function NewProduct() {
   const [fileName, setFileName] = useState(null)
   const [categories, setCategories] = useState([])
-  const { register, handleSubmit, control } = useForm()
+
+  const schema = Yup.object().shape({
+    name: Yup.string().required('Digite o nome do produto'),
+    price: Yup.string().required('Digite o preço do produto'),
+    category: Yup.object().required('Escolha uma categoria'),
+    file: Yup.mixed()
+      .test('required', 'Carregue um arquivo', value => {
+        return value?.length > 0
+      })
+      .test('filseSize', 'Carregue arquivos de até 2mb', value => {
+        return value[0]?.size <= 200000
+      })
+      .test('type', 'Carregue apenas arquivos JPEG ou PNG', value => {
+        return value[0]?.type === 'image/jpeg' || value[0]?.type === 'image/png'
+      })
+  })
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
+
   const onSubmit = data => console.log(data)
 
   useEffect(() => {
@@ -22,14 +50,18 @@ function NewProduct() {
     loadCategories()
   }, [])
 
+  console.log(fileName)
+
   return (
     <Container>
       <form noValidate onSubmit={handleSubmit(onSubmit)}>
         <Label>Nome</Label>
         <Input type="text" {...register('name')} />
+        <ErrorMessage>{errors.name?.message}</ErrorMessage>
 
         <Label>Preço</Label>
         <Input type="number" {...register('price')} />
+        <ErrorMessage>{errors.price?.message}</ErrorMessage>
 
         <LabelUpload>
           {fileName || (
@@ -41,15 +73,16 @@ function NewProduct() {
           <input
             type="file"
             accept="image/png, image/jpeg"
+            {...register('file')}
             onChange={value => {
               setFileName(value.target.files[0]?.name)
             }}
-            {...register('file')}
           />
         </LabelUpload>
+        <ErrorMessage>{errors.file?.message}</ErrorMessage>
 
         <Controller
-          name="category_id"
+          name="category"
           control={control}
           render={({ field }) => {
             return (
@@ -62,6 +95,7 @@ function NewProduct() {
             )
           }}
         ></Controller>
+        <ErrorMessage>{errors.category?.message}</ErrorMessage>
 
         <ButtonStyles>Adicionar produto</ButtonStyles>
       </form>
