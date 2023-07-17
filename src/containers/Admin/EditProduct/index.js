6702e318cbf7d53808a2746504a72351d82c1bc3
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-07-17 09:19:19 -0300

    feat: ending edit product screen

--- src/containers/Admin/EditProduct/index.js
+++ src/containers/Admin/EditProduct/index.js
@@ -9,8 +9,16 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload'
 import * as Yup from 'yup'
 
 import { ErrorMessage } from '../../../components'
+import paths from '../../../constants/paths'
 import api from '../../../services/api'
-import { Container, Label, LabelUpload, Input, ButtonStyles } from './styles'
+import {
+  Container,
+  Label,
+  LabelUpload,
+  Input,
+  ButtonStyles,
+  ContainerInput
+} from './styles'
 
 function EditProduct() {
   const [fileName, setFileName] = useState(null)
@@ -27,16 +35,7 @@ function EditProduct() {
     name: Yup.string().required('Digite o nome do produto'),
     price: Yup.string().required('Digite o preço do produto'),
     category: Yup.object().required('Escolha uma categoria'),
-    file: Yup.mixed()
-      .test('required', 'Carregue um arquivo', value => {
-        return value?.length > 0
-      })
-      .test('filseSize', 'Carregue arquivos de até 2mb', value => {
-        return value[0]?.size <= 200000
-      })
-      .test('type', 'Carregue apenas arquivos JPEG ou PNG', value => {
-        return value[0]?.type === 'image/jpeg' || value[0]?.type === 'image/png'
-      })
+    offer: Yup.bool()
   })
 
   const {
@@ -55,15 +54,19 @@ function EditProduct() {
     productDataFormData.append('price', data.price)
     productDataFormData.append('category_id', data.category.id)
     productDataFormData.append('file', data.file[0])
+    productDataFormData.append('offer', data.offer)
 
-    await toast.promise(api.post('products', productDataFormData), {
-      pending: 'Criando novo produto...',
-      success: 'Produto criado com sucesso',
-      error: 'Falha ao criar o produto'
-    })
+    await toast.promise(
+      api.post(`products/${product.id}`, productDataFormData),
+      {
+        pending: 'Editando novo produto...',
+        success: 'Produto editado com sucesso',
+        error: 'Falha ao editar o produto'
+      }
+    )
 
     setTimeout(() => {
-      navigate('/listar-produtos')
+      navigate(paths.Products)
     }, 2000)
   }
 
@@ -81,13 +84,21 @@ function EditProduct() {
       <form noValidate onSubmit={handleSubmit(onSubmit)}>
         <div>
           <Label>Nome</Label>
-          <Input type="text" {...register('name')} />
+          <Input
+            type="text"
+            {...register('name')}
+            defaultValue={product.name}
+          />
           <ErrorMessage>{errors.name?.message}</ErrorMessage>
         </div>
 
         <div>
           <Label>Preço</Label>
-          <Input type="number" {...register('price')} />
+          <Input
+            type="number"
+            {...register('price')}
+            defaultValue={product.price}
+          />
           <ErrorMessage>{errors.price?.message}</ErrorMessage>
         </div>
 
@@ -115,6 +126,7 @@ function EditProduct() {
           <Controller
             name="category"
             control={control}
+            defaultValue={product.category}
             render={({ field }) => {
               return (
                 <ReactSelect
@@ -122,6 +134,7 @@ function EditProduct() {
                   options={categories}
                   getOptionLabel={cat => cat.name}
                   placeholder="Categorias"
+                  defaultValue={product.category}
                 />
               )
             }}
@@ -129,7 +142,16 @@ function EditProduct() {
           <ErrorMessage>{errors.category?.message}</ErrorMessage>
         </div>
 
-        <ButtonStyles>Adicionar produto</ButtonStyles>
+        <ContainerInput>
+          <input
+            type="checkbox"
+            defaultChecked={product.offer}
+            {...register('offer')}
+          />
+          <Label>Produto em oferta?</Label>
+        </ContainerInput>
+
+        <ButtonStyles>Editar produto</ButtonStyles>
       </form>
     </Container>
   )
